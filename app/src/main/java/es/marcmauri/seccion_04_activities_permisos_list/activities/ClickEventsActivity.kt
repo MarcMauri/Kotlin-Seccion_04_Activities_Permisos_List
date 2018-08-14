e54067f719276023b29831c825fce3ef3db690bc
Marc from GitKraken <marcmauri92@gmail.com>
2018-08-14 12:29:58 +0200

    Click events activity

--- app/src/main/java/es/marcmauri/seccion_04_activities_permisos_list/activities/ClickEventsActivity.kt
+++ app/src/main/java/es/marcmauri/seccion_04_activities_permisos_list/activities/ClickEventsActivity.kt
@@ -2,12 +2,50 @@ package es.marcmauri.seccion_04_activities_permisos_list.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Button
 import es.marcmauri.seccion_04_activities_permisos_list.R
 
-class ClickEventsActivity : AppCompatActivity() {
+class ClickEventsActivity : AppCompatActivity(), View.OnLongClickListener {
+
+    // 1) Click en XML
+    // 2) Click en linea
+    // 3) Click by IDS en 'when'
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click_events)
+
+        clickInLine()
+
+        val btn1 = findViewById<Button>(R.id.buttonClickMulti1)
+        val btn2 = findViewById<Button>(R.id.buttonClickMulti2)
+        val btn3 = findViewById<Button>(R.id.buttonClickMulti3)
+
+        btn1.setOnLongClickListener(this)
+        btn2.setOnLongClickListener(this)
+        btn3.setOnLongClickListener(this)
     }
+
+    // Regla obligatoria para clicks en XMLs:
+    // el metodo tiene que ser publico, nombres tienen que coincidir y recibe 1 solo parametro (View)
+    fun xmlClick(view: View) {
+        Snackbar.make(view, "Click by XML!", Snackbar.LENGTH_SHORT).show()
+    }
+
+    private fun clickInLine() {
+        val btn = findViewById<Button>(R.id.buttonClickInLine)
+        btn.setOnClickListener { view -> Snackbar.make(view, "Click in Line!", Snackbar.LENGTH_SHORT).show() }
+    }
+
+    override fun onLongClick(view: View): Boolean {
+        when (view.id) {
+            R.id.buttonClickMulti1 -> Snackbar.make(view, "Click Multi 1!", Snackbar.LENGTH_SHORT).show()
+            R.id.buttonClickMulti2 -> Snackbar.make(view, "Click Multi 2!", Snackbar.LENGTH_SHORT).show()
+            R.id.buttonClickMulti3 -> Snackbar.make(view, "Click Multi 3!", Snackbar.LENGTH_SHORT).show()
+        }
+        return true
+    }
+
 }
