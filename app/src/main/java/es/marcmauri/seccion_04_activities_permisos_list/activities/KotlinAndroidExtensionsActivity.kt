3f2188b13b72adec497a9cabaa66b33384ec9a96
Marc from GitKraken <marcmauri92@gmail.com>
2018-08-14 12:37:59 +0200

    Kotlin Android Extensions

--- app/src/main/java/es/marcmauri/seccion_04_activities_permisos_list/activities/KotlinAndroidExtensionsActivity.kt
+++ app/src/main/java/es/marcmauri/seccion_04_activities_permisos_list/activities/KotlinAndroidExtensionsActivity.kt
@@ -2,12 +2,24 @@ package es.marcmauri.seccion_04_activities_permisos_list.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.widget.Button
 import es.marcmauri.seccion_04_activities_permisos_list.R
+import kotlinx.android.synthetic.main.activity_kotlin_android_extensions.*
 
 class KotlinAndroidExtensionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_android_extensions)
+
+        val btn = findViewById<Button>(R.id.buttonById)
+        btn.setOnClickListener { view ->
+            Snackbar.make(view, "Click By ID!", Snackbar.LENGTH_SHORT).show()
+        }
+
+        buttonByKAT.setOnClickListener { view ->
+            Snackbar.make(view, "Click By KAT!", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
