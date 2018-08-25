1199c46cab0b978e409bc38d121eee73a602cc6b
Marc from GitKraken <marcmauri92@gmail.com>
2018-08-25 18:10:03 +0200

    Consumo de Shared Preferences

--- app/src/main/java/es/marcmauri/seccion_04_activities_permisos_list/activities/SharedPreferencesActivity.kt
+++ app/src/main/java/es/marcmauri/seccion_04_activities_permisos_list/activities/SharedPreferencesActivity.kt
@@ -1,6 +1,7 @@
 package es.marcmauri.seccion_04_activities_permisos_list.activities
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.Toolbar
 import es.marcmauri.seccion_04_activities_permisos_list.R
 import es.marcmauri.seccion_04_activities_permisos_list.app.preferences
@@ -14,6 +15,29 @@ class SharedPreferencesActivity : ToolbarActivity() {
         setContentView(R.layout.activity_shared_preferences)
         toolbarToLoad(toolbar as Toolbar)
 
-        preferences.name = ""
+        buttonSave.setOnClickListener { setValuesSharedPreferences(); cleanEditTexts(); getValuesSharedPreferences() }
+        getValuesSharedPreferences()
+    }
+
+    private fun getValuesSharedPreferences() {
+        if (preferences.name.isNotEmpty() && preferences.age >= 0)
+            textViewSharedPreferences.text = "Welcome ${preferences.name}, your age is ${preferences.age}"
+        else
+            textViewSharedPreferences.text = "Welcome! Please save your name and age"
+    }
+
+    private fun setValuesSharedPreferences() {
+        if (editTextName.text.toString().isNotEmpty() && editTextAge.toString().isNotEmpty()) {
+            preferences.name = editTextName.text.toString()
+            preferences.age = editTextAge.text.toString().toInt()
+            Snackbar.make(constraint, "Values have been saved successfully", Snackbar.LENGTH_SHORT).show()
+        } else {
+            Snackbar.make(constraint, "Please fill the name and the age before saving", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun cleanEditTexts() {
+        editTextName.text.clear()
+        editTextAge.setText("")
     }
 }
